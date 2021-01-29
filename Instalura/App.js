6b3d9535b22783d55f8cf5e1ae99ae7f4efb6742
Rodrigo Peddinghausen <rodpedd@gmail.com>
2021-01-29 11:33:53 -0300

    Base of the project. Setting it up was a nightmare... thanks to a certein big sur... :see_no_evil:

---
+++ Instalura/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {Text} from "react-native";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>John</Text>
+      <Text>Paula</Text>
+    </Fragment>
+  );
+};
+
+
+
+export default App;
